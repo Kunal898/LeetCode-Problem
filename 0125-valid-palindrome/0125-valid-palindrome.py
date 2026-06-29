79be83b77285a480a3d9bb1810533905783823b6
Kunal navdhinge <166474107+Kunal898@users.noreply.github.com>
2026-06-29 16:13:23 +0530

    Time: 3 ms (99%), Space: 19.9 MB (36.14%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,7 @@
+import string
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.translate(s.maketrans('', '', string.punctuation)).lower()
+        s = s.translate(s.maketrans('', '', string.whitespace))
+        return s == s[::-1]
